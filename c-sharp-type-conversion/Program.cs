84305f1f6f78eb093203a2ca1ddc69397a3f2a29
jkchristensen36 <jkchristensen36@gmail.com>
2020-10-05 16:36:40 -0700

    Initialized practice

---
+++ c-sharp-type-conversion/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace TypeConversion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 1;
+            byte b = (byte) i;
+            Console.WriteLine(b);
+        }
+    }
+}
